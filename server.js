aaba3148e6a82dbdb9c4aebea44489fc37d273ef
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-11 00:28:20 -0700

    set up the inquire.prompt that will ask the user the desired questions to set up the employee tracker,made my array of questions that the user can choose and begining the .then functions, will pick it back up tomorrow

--- server.js
+++ server.js
@@ -8,7 +8,34 @@ const connection = mysql.createConnection({
     user        :   'root',
     password    :   "",
     databasse   :   "employeesDB"
+});
 
- 
+//here we are going to begin the connection to mysql 
+connection.connect(function (err) {
+    if (err) throw err;
 
+    startApp();
+});
+
+// Here is the set up to start the questions for the user
+inquirer.prompt({
+    name: 'startQuestions',
+    type: 'list',
+    message: 'How shall you begin?', 
+    choices: ['Exit','Add another employee', 'Delete an employee','Add another role','See all employees', 
+    'Search for an employee', 'View employee by department', 'View employee by role']
+}).then(function(response) {
+
+    switch (response.startQuestions) {
+        case "Exit":
+           connection.end();
+           break; 
+        
+        case "Add another employee":
+            addEmployee();
+            break;
+        case   "Delete an employee"
+            deleteEmployee();
+            break; 
+        }
 });
