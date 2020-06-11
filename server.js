e07f118092b78254e68c04c30a751a91993e5cb2
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-10 23:55:37 -0700

    set up the mysql connection in the server.js file

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-const inquirer = require("inquirer");
-const mysql = require("mysql");
+// Here are the dependencies for the project
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+// setting up the mysql database connection
+const connection = mysql.createConnection({
+    host        :   'localhost',
+    user        :   'root',
+    password    :   "",
+    databasse   :   "employeesDB"
+
+ 
+
+});
