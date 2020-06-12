984654f735aa86e39c69396f2dbe47b943df0ee8
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-11 22:58:57 -0700

    added missing line to set up port 3306 the default for mysql, added line 21 to complete the startApp function, also completed the .then response function for each user question choice

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mysql = require('mysql');
 // setting up the mysql database connection
 const connection = mysql.createConnection({
     host        :   'localhost',
+    port        :   '3306',  
     user        :   'root',
     password    :   "",
     databasse   :   "employeesDB"
@@ -13,9 +14,11 @@ const connection = mysql.createConnection({
 //here we are going to begin the connection to mysql 
 connection.connect(function (err) {
     if (err) throw err;
-
+    //begins the app
     startApp();
 });
+
+function startApp();
 
 // Here is the set up to start the questions for the user
 inquirer.prompt({
@@ -34,8 +37,28 @@ inquirer.prompt({
         case "Add another employee":
             addEmployee();
             break;
-        case   "Delete an employee"
+        case   "Delete an employee":
             deleteEmployee();
-            break; 
+            break;
+
+        case   "Add another role":
+            addRole();
+            break;
+
+        case   "See all employees":
+                seeEmployees();
+                break;
+
+        case   "Search for an employee":
+                    locateEmployee();
+                    break;
+
+        case   "View employee by department":
+                        viewEmployee_Dartment();
+                        break;
+
+        case   "View employee by role":
+                            viewEmployee_Role();
+                            break;                
         }
 });
