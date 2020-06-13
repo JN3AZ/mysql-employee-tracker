a34c7d868e70d0b462f20820402bd638189efe3c
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-12 23:39:58 -0700

    fixing a few bugs I found like missing the s in employeesDB on the seeds.sql file also making adjustments to the id numbers of each area trying to resolve an error im getting in mysql about duplicate id's

--- seeds.sql
+++ seeds.sql
@@ -1,32 +1,32 @@
-USE employeeDB;
+USE employeesDB;
 
 INSERT INTO department (id, moniker)
 VALUES
-(1, "Management"),
-(2, "Engineering"),
-(3, "Sales"),
-(4, "Finance"),
-(5, "Legal");
+(001, "Management"),
+(002, "Engineering"),
+(003, "Sales"),
+(004, "Finance"),
+(005, "Legal");
 
 INSERT INTO employeeRole (id, title, salary, department_id)
 VALUES
-(1, "Owner/CEO", 1000000, 1),
-(2, "Chief of Operations", 300000, 1),
-(3, "Lead Engineer", 225000, 2),
-(4, "Junior Developer", 95000, 2),
-(5,"Head of Sales", 90000, 3),
-(6,"Sales Lead", 75000, 3),
-(7, "Lead-Accountant", 90000, 4),
-(8, "Company Lawyer", 100000, 5);
+(10, "Owner/CEO", 1000000, 1),
+(20, "Chief of Operations", 300000, 1),
+(30, "Lead Engineer", 225000, 2),
+(40, "Junior Developer", 95000, 2),
+(50,"Head of Sales", 90000, 3),
+(60,"Sales Lead", 75000, 3),
+(70, "Lead-Accountant", 90000, 4),
+(80, "Company Lawyer", 100000, 5);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
 (1, "Elon", "Musk",1, 1),
-(1, "Sherl", "Sandberg",2, 1),
-(1, "John", "Carmack",3, 3),
-(1, "Bill", "Gates",4, 3),
-(1, "Jeffery", "Lebowski",5, 2),
-(1, "Dwight", "Schrute",6, 2),
-(1, "Calvin", "Joyner",7, 4),
-(1, "Saul", "Goodman",8, 5),
+(2, "Sherl", "Sandberg",2, 1),
+(3, "John", "Carmack",3, 3),
+(4, "Bill", "Gates",4, 3),
+(5, "Jeffery", "Lebowski",5, 2),
+(6, "Dwight", "Schrute",6, 2),
+(7, "Calvin", "Joyner",7, 4),
+(8, "Saul", "Goodman",8, 5);
 
