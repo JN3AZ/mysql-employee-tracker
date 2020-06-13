6a3b5b0f9ff4b082b2e4b908abd767ae8a99bab2
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-12 23:59:21 -0700

    forgot to save before pushing last with a few minor edits adding null to employees that dont have a manager and changing the separtment_id on the employeeRole

--- seeds.sql
+++ seeds.sql
@@ -10,23 +10,23 @@ VALUES
 
 INSERT INTO employeeRole (id, title, salary, department_id)
 VALUES
-(10, "Owner/CEO", 1000000, 1),
-(20, "Chief of Operations", 300000, 1),
-(30, "Lead Engineer", 225000, 2),
-(40, "Junior Developer", 95000, 2),
-(50,"Head of Sales", 90000, 3),
-(60,"Sales Lead", 75000, 3),
-(70, "Lead-Accountant", 90000, 4),
-(80, "Company Lawyer", 100000, 5);
+(10, "Owner/CEO", 1000000, 001),
+(20, "Chief of Operations", 300000, 001),
+(30, "Lead Engineer", 225000, 002),
+(40, "Junior Developer", 95000, 002),
+(50,"Head of Sales", 90000, 003),
+(60,"Sales Lead", 75000, 003),
+(70, "Lead-Accountant", 90000, 004),
+(80, "Company Lawyer", 100000, 005);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
-(1, "Elon", "Musk",1, 1),
+(1, "Elon", "Musk",1, Null),
 (2, "Sherl", "Sandberg",2, 1),
-(3, "John", "Carmack",3, 3),
-(4, "Bill", "Gates",4, 3),
+(3, "John", "Carmack",3, Null),
+(4, "Bill", "Gates",4, 1),
 (5, "Jeffery", "Lebowski",5, 2),
 (6, "Dwight", "Schrute",6, 2),
-(7, "Calvin", "Joyner",7, 4),
-(8, "Saul", "Goodman",8, 5);
+(7, "Calvin", "Joyner",7, 3),
+(8, "Saul", "Goodman",8, Null);
 
